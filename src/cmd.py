8d1d718fc5bda6e16e588743d8276d6752c3e4e5
Gary Lu <wklu99@gmail.com>
2020-07-30 18:06:26 +0200

    Added more arguments for cmd
    Added source, destination, and visualisation arguments to cmd.

--- src/cmd.py
+++ src/cmd.py
@@ -9,9 +9,24 @@ def parse():
     parser.add_argument("-a",
                         "--algorithm",
                         choices=algorithm_list,
+                        required=True,
                         help="Select a clustering algorithm",)
+    parser.add_argument("-s",
+                        "--source",
+                        default="input/location",  # change
+                        help="Select the input source",)
+    parser.add_argument("-d",
+                        "--destination",
+                        default="output/destination",  # change
+                        help="Select output destination",)
+    parser.add_argument("-v",
+                        "--visualise",
+                        type=bool,
+                        choices=[True, False],
+                        default=False,
+                        help="Select whether to visualise cluster results",)
     args = parser.parse_args()
-    print(args)
+    print(args)  # keep for debugging args
 
 
 if __name__ == "__main__":
