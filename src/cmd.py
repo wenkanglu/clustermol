d1816c4cc882e8945d6c05bceca4e62f51b2c4a4
Gary Lu <wklu99@gmail.com>
2020-08-10 19:20:28 +0200

    Added simple .ini parsing to automate user arguments
    Added configs, data_src, data_dest directories for testing. cmd.py now
    takes a -c flag for a configuration file.

--- src/cmd.py
+++ src/cmd.py
@@ -1,4 +1,13 @@
 import argparse
+import configparser
+import os
+
+
+# print(os.getcwd())
+config_path = os.path.join("configs", "testconfig.ini")
+# print(config_path)
+config = configparser.ConfigParser()
+config.read(config_path)
 
 algorithm_list = ["alg1", "alg2", "alg3"]
 
@@ -6,10 +15,15 @@ algorithm_list = ["alg1", "alg2", "alg3"]
 def parse():
     parser = argparse.ArgumentParser()
     # use group = parser.add_mutually_exclusive_group() if needed
+    parser.add_argument("-c",
+                        "--configuration",
+                        type=bool,
+                        choices=[True, False],
+                        default=False,
+                        help="Select configuration file/s", )
     parser.add_argument("-a",
                         "--algorithm",
                         choices=algorithm_list,
-                        required=True,
                         help="Select a clustering algorithm",)
     parser.add_argument("-s",
                         "--source",
@@ -27,6 +41,12 @@ def parse():
                         help="Select whether to visualise cluster results",)
     args = parser.parse_args()
     print(args)  # keep for debugging args
+
+    if args.configuration:
+        for section in config.sections():
+            print(section)
+            args.source = config[section]["--source"]  # sets source from config file
+            print(args.source)
 
 
 if __name__ == "__main__":
