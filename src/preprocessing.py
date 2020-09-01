0ecf9abd002b88d237c57d8dce7abe8237e429f7
Nicholas Limbert <niclimbert@gmail.com>
2020-09-01 11:54:52 +0200

    Pre-proccesing clean up.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -16,8 +16,6 @@ def clean_trajectory(traj):
     Return:
         trajectory (mdtraj.Trajectory): cleaned trajectory object.
     '''
-    # sel = traj.topology.select("resname != SOD")
-    # traj = traj.atom_slice(sel)
     return traj.remove_solvent()
 
 def preprocessing_file(filename):
@@ -32,22 +30,14 @@ def preprocessing_file(filename):
     '''
     # Change to directroy for input file source.
     # Note data should always be placed in the data_src file before running.
-    os.chdir(os.path.join(os.path.dirname(__file__), '..')+"/data/data_src")  # changes cwd to always be at clustermol
-
-    # Load Trajectory
+    os.chdir(os.path.join(os.path.dirname(__file__), '..')+"/data/data_src")
     print(">>> Loading file \"%s\"" %filename)
     trajectory = mdtraj.load(filename)
-    # Illustrate that all Frames are loaded
     print(">>> File loaded")
-    # print(trajectory[::4])
-    # trajectory = trajectory[::4]
-    # trajectory.save("MenY_aligned_downsamp10_reduced(Nic).pdb")
-    trajectory = clean_trajectory(trajectory) # Cleans trajectory method
+    trajectory = clean_trajectory(trajectory)
     print(">>>", trajectory)
-    # print(">>> All atoms: %s" % [atom for atom in trajectory.topology.atoms])
-    os.chdir(os.path.join(os.path.dirname(__file__), '..')) # change back to clustermol root directory.
+    os.chdir(os.path.join(os.path.dirname(__file__), '..'))
     return trajectory
-
 
 def preprocessing_hierarchical(traj):
     '''
@@ -60,30 +50,14 @@ def preprocessing_hierarchical(traj):
     Return:
         rmsd_matrix (numpy.np): rmsd matrix for clustering.
     '''
-    # Calculate RMSD Pairwsie Matrix
-    ## superpose atomset 1 to reference
-
     rmsd_matrix = numpy.ndarray((traj.n_frames, traj.n_frames), dtype=numpy.float64)
     for i in range(traj.n_frames):
-        rmsd_ = mdtraj.rmsd(traj, traj, i, parallel=True) #currently we assume they are pre-centered, but can they not be?
+        rmsd_ = mdtraj.rmsd(traj, traj, i, parallel=True)
         rmsd_matrix[i] = rmsd_
-    # print('Max pairwise rmsd: %f nm' % np.max(rmsd_matrix))
     print('>>> RMSD matrix complete')
-    # postprocessing.illustrateRMSD(rmsd_matrix)
-    # postprocessing.illustrateRMSD(rmsd_matrix)
-    # file1 = open("matrixOutput.txt","w")
-    # np.set_printoptions(threshold=np.inf)
-    # file1.write(np.array2string(rmsd_matrix))
-
-    # We can check that the diagonals are all zero using the below statetemnt, however checks vis squareform fail
-    # so it is set to false. squareform checks are too stringent
-    # print(np.diag(rmsd_matrix))
-
-    # Clean up and Preprocessing of Matrix
     # assert numpy.all(rmsd_matrix - rmsd_matrix.T < 1e-6) # Need to figure out what this is for.
     reduced_distances = squareform(rmsd_matrix, checks=False)
     return reduced_distances
-    # return rmsd_matrix
 
 def preprocessing_qt(traj):
     '''
@@ -95,10 +69,9 @@ def preprocessing_qt(traj):
     Return:
         rmsd_matrix (numpy.np): rmsd matrix for clustering.
     '''
-    # Calculate RMSD Pairwsie Matrix
     rmsd_matrix = numpy.ndarray((traj.n_frames, traj.n_frames), dtype=numpy.float64)
     for i in range(traj.n_frames):
-        rmsd_ = mdtraj.rmsd(traj, traj, i, parallel=True) # currently we assume they are pre-centered, but can they not be?
+        rmsd_ = mdtraj.rmsd(traj, traj, i, parallel=True)
         rmsd_matrix[i] = rmsd_
     print('>>> RMSD matrix complete')
     return rmsd_matrix
@@ -113,7 +86,6 @@ def getRMSD_first_frame(traj):
     Return:
         rmsd_matrix (numpy.np): rmsd matrix for visualization.
     '''
-    # Calculate RMSD Pairwsie Matrix
     rmsd_matrix = numpy.ndarray((traj.n_frames, traj.n_frames), dtype=numpy.float64)
     rmsd_matrix = mdtraj.rmsd(traj, traj, 0)
     return rmsd_matrix
